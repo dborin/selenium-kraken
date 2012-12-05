1a0c75cbb3f119d6a5bb6bdcfe95a28b9ea4ee82
David Borin <david.borin@rackspace.com>
2012-12-05 12:48:14 -0800

    adding node files

--- bootstrap.py
+++ bootstrap.py
@@ -1,33 +1,39 @@
+#!/usr/bin/env python
+
 from optparse import OptionParser
 from littlechef import runner as lc
 from ssh.config import SSHConfig as _SSHConfig
 import os
 
-parser = OptionParser()
-parser.add_option("-p", "--password", dest="password",
-    help="root password for new server")
-parser.add_option("-H", "--hostname", dest="hostname",
-    help="server hostname")
-parser.add_option("-r", "--role", dest="role",
-    type="choice", choices=["hub", "node"],
-    default="node",
-    help="Type of server (hub or node) [default: %default]")
+def main():
+    parser = OptionParser()
+    parser.add_option("-p", "--password", dest="password",
+        help="root password for new server")
+    parser.add_option("-H", "--hostname", dest="hostname",
+        help="server hostname")
+    parser.add_option("-r", "--role", dest="role",
+        type="choice", choices=["hub", "node"],
+        default="node",
+        help="Type of server (hub or node) [default: %default]")
 
-(options, args) = parser.parse_args()
+    (options, args) = parser.parse_args()
 
-# begin hackery to load config file until littlechef gets fixed (04DEC2012)
+    # begin hackery to load config file until littlechef gets fixed (04DEC2012)
 
-ssh_config = "~/.ssh/chef.config"
-lc.env.use_ssh_config = True
-lc.env.ssh_config = _SSHConfig()
-lc.env.ssh_config_path = os.path.expanduser(ssh_config)
+    ssh_config = "~/.ssh/chef.config"
+    lc.env.use_ssh_config = True
+    lc.env.ssh_config = _SSHConfig()
+    lc.env.ssh_config_path = os.path.expanduser(ssh_config)
 
-# end hackery
+    # end hackery
 
-lc.env.user = 'root'
-lc.env.password = options.password
-lc.env.host_string = options.hostname
-lc.env.host = options.hostname
-lc.deploy_chef()
-lc.node(options.hostname)
-lc.role("se-%s" % options.role)
+    lc.env.user = 'root'
+    lc.env.password = options.password
+    lc.env.host_string = options.hostname
+    lc.env.host = options.hostname
+    lc.deploy_chef()
+    lc.node(options.hostname)
+    lc.role("se-%s" % options.role)
+
+if __name__=="__main__":
+    main()
