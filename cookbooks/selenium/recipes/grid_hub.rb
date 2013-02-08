4543197c8968e8689b39231b2104b9d2293262c4
David Borin <david.borin@rackspace.com>
2013-02-07 16:33:27 -0800

    removed init.d, added runit cookbook and runit services

--- cookbooks/selenium/recipes/grid_hub.rb
+++ cookbooks/selenium/recipes/grid_hub.rb
@@ -1,5 +1,6 @@
 include_recipe 'selenium::default'
 include_recipe 'java'
+include_recipe 'runit'
 
 template File.join(node['selenium']['server']['installpath'], 'hubConfig.json') do
   source "hubConfig.json.erb"
@@ -8,42 +9,6 @@ template File.join(node['selenium']['server']['installpath'], 'hubConfig.json') 
   group node['selenium']['user']
 end
 
-SERVNAME='run-hub'
-script_path=node['selenium']['home']+'init/'+SERVNAME+'.sh'
-
-template script_path do
-  source SERVNAME+'.erb'
-  mode 0755
-  owner node['selenium']['user']
-  group node['selenium']['user']
-end
-
-link "/etc/init.d/"+SERVNAME do
-  to script_path
-end
-
-case node["platform"]
-  when "centos","redhat","fedora"
-     execute "update_rc_for_reg-agent" do
-	user "root"
-	group "root"
-	cwd "/etc/init.d"
-	command <<-EOH
-        chkconfig --add #{SERVNAME} || echo 'service #{SERVNAME} already registred'
-	EOH
-     end
-  when "debian","ubuntu"
-     execute "update_rc_for_reg-agent" do
-	user "root"
-	group "root"
-	cwd "/etc/init.d"
-	command <<-EOH
-        update-rc.d #{SERVNAME} defaults 98 02 || echo 'service #{SERVNAME} already registred'
-	EOH
-    end
-end
-
-service SERVNAME do
-  supports :restart => true
-  action :restart
+runit_service "selenium-hub" do
+  default_logger true
 end
--- cookbooks/selenium/recipes/grid_node.rb
+++ cookbooks/selenium/recipes/grid_node.rb
@@ -1,6 +1,7 @@
 include_recipe 'selenium::default'
 include_recipe 'java'
 include_recipe 'selenium::xvfb'
+include_recipe 'runit'
 
 
 template File.join(node['selenium']['server']['installpath'], 'nodeConfig.json') do
@@ -10,42 +11,6 @@ template File.join(node['selenium']['server']['installpath'], 'nodeConfig.json')
   group node['selenium']['user']
 end
 
-SERVNAME='run-node'
-script_path=node['selenium']['home']+'init/'+SERVNAME+'.sh'
-
-template script_path do
-  source SERVNAME+'.erb'
-  mode 0755
-  owner node['selenium']['user']
-  group node['selenium']['user']
-end
-
-link "/etc/init.d/"+SERVNAME do
-  to script_path
-end
-
-case node["platform"]
-  when "centos","redhat","fedora"
-     execute "update_rc_for_reg-agent" do
-	user "root"
-	group "root"
-	cwd "/etc/init.d"
-	command <<-EOH
-        chkconfig --add #{SERVNAME} || echo 'service #{SERVNAME} already registred'
-	EOH
-     end
-  when "debian","ubuntu"
-     execute "update_rc_for_reg-agent" do
-	user "root"
-	group "root"
-	cwd "/etc/init.d"
-	command <<-EOH
-        update-rc.d #{SERVNAME} defaults 98 02 || echo 'service #{SERVNAME} already registred'
-	EOH
-    end
-end
-
-service SERVNAME do
-  supports :restart => true
-  action :restart
+runit_service "selenium-node" do
+  default_logger true
 end
