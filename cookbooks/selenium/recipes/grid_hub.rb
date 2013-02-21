05d98d4731c3d5870aa7d144cdd9f507d66ad11c
David Borin <david.borin@rackspace.com>
2013-02-21 14:16:07 -0800

    had to move default into grid-hub and grid-node to ensure restart after selenium jar file update

--- cookbooks/selenium/recipes/grid_hub.rb
+++ cookbooks/selenium/recipes/grid_hub.rb
@@ -1,6 +1,37 @@
-include_recipe 'selenium::default'
 include_recipe 'java'
 include_recipe 'runit'
+
+USER=node['selenium']['user']
+
+group USER
+
+service "selenium-hub"
+
+user USER do
+  comment "selenium user"
+  gid USER
+  #system true
+  shell "/bin/bash"
+  home node['selenium']['home']
+end
+
+directory node['selenium']['server']['installpath'] do
+  owner USER
+  recursive true
+end
+
+remote_file File.join(node['selenium']['server']['installpath'], "selenium-server-standalone-#{node['selenium']['server']['version']}.jar") do
+  source "http://selenium.googlecode.com/files/selenium-server-standalone-#{node['selenium']['server']['version']}.jar"
+  action :create_if_missing
+  mode 0644
+  owner USER
+  group USER
+  notifies :restart, resources(:service => ["selenium-hub"])
+end
+
+link File.join(node['selenium']['server']['installpath'], 'selenium-server-standalone.jar') do
+  to File.join(node['selenium']['server']['installpath'], "selenium-server-standalone-#{node['selenium']['server']['version']}.jar")
+end
 
 template File.join(node['selenium']['server']['installpath'], 'hubConfig.json') do
   source "hubConfig.json.erb"
--- cookbooks/selenium/recipes/grid_node.rb
+++ cookbooks/selenium/recipes/grid_node.rb
@@ -1,8 +1,40 @@
-include_recipe 'selenium::default'
 include_recipe 'java'
 include_recipe 'selenium::xvfb'
 include_recipe 'runit'
 
+USER=node['selenium']['user']
+
+group USER
+
+user USER do
+  comment "selenium user"
+  gid USER
+  #system true
+  shell "/bin/bash"
+  home node['selenium']['home']
+end
+
+directory node['selenium']['server']['installpath'] do
+  owner USER
+  recursive true
+end
+
+runit_service "selenium-node" do
+  default_logger true
+end
+
+remote_file File.join(node['selenium']['server']['installpath'], "selenium-server-standalone-#{node['selenium']['server']['version']}.jar") do
+  source "http://selenium.googlecode.com/files/selenium-server-standalone-#{node['selenium']['server']['version']}.jar"
+  action :create_if_missing
+  mode 0644
+  owner USER
+  group USER
+  notifies :restart, resources(:runit_service => ["selenium-node"])
+end
+
+link File.join(node['selenium']['server']['installpath'], 'selenium-server-standalone.jar') do
+  to File.join(node['selenium']['server']['installpath'], "selenium-server-standalone-#{node['selenium']['server']['version']}.jar")
+end
 
 template File.join(node['selenium']['server']['installpath'], 'nodeConfig.json') do
   source "nodeConfig.json.erb"
@@ -11,6 +43,3 @@ template File.join(node['selenium']['server']['installpath'], 'nodeConfig.json')
   group node['selenium']['user']
 end
 
-runit_service "selenium-node" do
-  default_logger true
-end
