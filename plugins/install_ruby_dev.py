23c8e815715a44d3572c3ae1d37782b249f143cb
David Borin <borin8765+github@gmail.com>
2016-02-02 10:48:31 -0800

    update

--- plugins/install_ruby_dev.py
+++ plugins/install_ruby_dev.py
@@ -2,4 +2,4 @@ from fabric.api import sudo
 #from littlechef import runner as lc
 
 def execute(node):
-    sudo('apt-get -y install ruby1.9.1 ruby-dev')
+    sudo('apt-get -y install ruby1.9.1 ruby-dev dbus-x11')
